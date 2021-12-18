bd4815fc774bf9e27c5443ebaf48ba96cacff1ac
ansh <ansh-sangwan02@gmail.com>
2021-12-18 20:36:24 +0530

    trayal javascript

---
+++ JAVASCRIPT/manval.js
@@ -0,0 +1,55 @@
+console.log("hello world");
+
+// var myname ="anshsangwan";
+// var myage = 14;
+// console.log(myname);
+// console.log(myage);
+
+//  naming practice
+
+// var  _myname = "ansh";
+// var 1myname = "sangwan";
+// var _1my__name = "nitin";
+// var $myname = "nisha";
+// console.log($myname);
+
+// DATA TYPE IN JAVASCRIPT
+
+// Six data type that are primitive,
+
+// 1. undefined : typeof instance === "undefined";
+// 2. boolean : type of instance === "boolean";
+// 3. number :type of instance === "number";
+// 4. string : type of instance === "string";
+// 5. biglnt : type of instance === "bigint";
+// 6. symbol : type of instance === "symbol";
+
+// var myname = "anshsangwan";
+// console.log(myname);
+// console.log(typeof(myname));
+//  var myage = 14;
+//  console.log(myage);
+//  console.log(typeof(myage));
+//  var iAmanshs =false;
+// console.log(iAmanshs);
+// console.log(typeof(iAmanshs));
+
+// datatype practice
+
+// 10 +"20"
+// 9 - "5" bug
+// "java" + "script"
+// "  " +  "  "
+// "  " + 0
+// "ansh" - "sangwan"
+// true + true
+// true + false
+// false + true 
+// false - true
+
+console.log(false - true);
+
+
+
+
+
