147520c9658a0b7db7046c9321e03dde0adbac63
ansh <ansh-sangwan02@gmail.com>
2021-12-26 19:53:33 +0530

    nothing

--- JAVASCRIPT/manval.js
+++ JAVASCRIPT/manval.js
@@ -36,7 +36,36 @@ console.log("hello world");
 
 // datatype practice
 
-// 10 +"20"
+// `${
+//     10}20`
+// var myname ="anshsangwan";
+// var myage = 14;
+// console.log(myname);
+// console.log(myage);
+//  naming practice
+// var  _myname = "ansh";
+// var 1myname = "sangwan";
+// var _1my__name = "nitin";
+// var $myname = "nisha";
+// console.log($myname);
+// DATA TYPE IN JAVASCRIPT
+// Six data type that are primitive,
+// 1. undefined : typeof instance == "undefined";
+// 2. boolean : type of instance === "boolean";
+// 3. number :type of instance === "number";
+// 4. string : type of instance === "string";
+// 5. biglnt : type of instance === "bigint";
+// 6. symbol : type of instance === "symbol";
+// var myname = "anshsangwan";
+// console.log(myname);
+// console.log(typeof(myname));
+//  var myage = 14;
+//  console.log(myage);
+//  console.log(typeof(myage));
+//  var iAmanshs =false;
+// console.log(iAmanshs);
+// console.log(typeof(iAmanshs));
+// datatype practice
 // 9 - "5" bug
 // "java" + "script"
 // "  " +  "  "
@@ -47,19 +76,20 @@ console.log("hello world");
 // false + true 
 // false - true
 
-// console.log(false - true);
+// console.log(false == true);
 
 
 // difference beteen null vs undefined
 
-var iAmuseless = null ;
-console.log(iAmuseless);
-console.log(typeof(iAmuseless));
+// var iAmuseless = null ;
+// console.log(iAmuseless);
+// console.log(typeof(iAmuseless));
 
-var iAmStandBy ;
-console.log(iAmStandBy);
-console.log(typeof(iAmStandBy));
+// var iAmStandBy ;
+// console.log(iAmStandBy);
+// console.log(typeof(iAmStandBy));
 
+// va
 
 
 
@@ -67,28 +97,33 @@ console.log(typeof(iAmStandBy));
 // what is nan
 
 
-//    nan is a property of global Object.
+//    nan is a property of global Object.!=
 // in other words ,it is a variable in global scope
 // the initial value of nan is not-A-number
 
+// var myPhoneNumber = 9312845621;
+// var myName ="anshsangwan"
+// console.log(myPhoneNumber);
+// console.log(myName);
+// console.log(isNaN(myPhoneNumber));
+// console.log(isNaN(myName));
+
+// if (isNaN(myName)){
+//     console.log("plz enter valid phone no.");
+// }
+
+// NaN pratice
 
 
+//  var nan
 
 
+var num1 =5;
+var num2= 6;
+console.log(typeof(num1));
+console.log(typeof(num2));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(num1 != num2);
 
 
 
